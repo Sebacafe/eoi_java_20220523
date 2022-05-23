c6a895a60b942832ff28caf593bdbb872e70aa37
Sebacafe <seba_bonare@hotmail.com>
2022-05-23 17:33:30 +0200

    Main.java practica_1

---
+++ Counter.java
@@ -0,0 +1,10 @@
+public class Counter {
+        public static void contar(){
+        int numero = 0;
+        for (int i = 0; i <= 1000; i++) {
+            System.out.println("i = " + i);
+        }
+
+    }
+}
+
